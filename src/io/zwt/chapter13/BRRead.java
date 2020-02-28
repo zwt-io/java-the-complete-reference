6e1a32a95f5f914582a78f00e26499ae23b32dc8
招文桃 <zhao@wen-tao.com>
2020-02-28 17:04:22 +0800

    update: BBRead.java 更新一下英文注释。

--- src/io/zwt/chapter13/BRRead.java
+++ src/io/zwt/chapter13/BRRead.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// Use a BufferedReader to read characters from the console.
+
 public class BRRead {
     public static void main(String[] args) throws IOException {
         char c;
